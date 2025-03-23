a8d67d4be3458323c778d7a4183dead4bbfbaf8a
Eisenwave <me@eisenwave.net>
2025-03-23 11:46:13 +0100

    implement API functions

--- src/main/cpp/ulight.cpp
+++ src/main/cpp/ulight.cpp
@@ -2,8 +2,26 @@
 #include <new>
 #include <string_view>
 
+#include "ulight/impl/memory.hpp"
 #include "ulight/ulight.h"
 #include "ulight/ulight.hpp"
+
+#include "ulight/impl/highlight.hpp"
+#include "ulight/impl/unicode.hpp"
+
+namespace ulight {
+namespace {
+
+ulight::Highlight_Options to_options(ulight_flag flags) noexcept
+{
+    return {
+        .coalescing = (flags & ULIGHT_COALESCE) != 0,
+        .strict = (flags & ULIGHT_STRICT) != 0,
+    };
+}
+
+} // namespace
+} // namespace ulight
 
 extern "C" {
 
@@ -68,7 +86,7 @@ ulight_lang ulight_get_lang(const char* name, size_t name_length) noexcept
         : ULIGHT_LANG_NONE;
 }
 
-ulight_string_view ulight_highlight_type_id(ulight_highlight_type type)
+ulight_string_view ulight_highlight_type_id(ulight_highlight_type type) noexcept
 {
     const std::string_view result = ulight::ulight_highlight_type_id(ulight::Highlight_Type(type));
     return { result.data(), result.size() };
@@ -125,24 +143,108 @@ void ulight_delete(ulight_state* state) noexcept
 
 ulight_status ulight_source_to_tokens(ulight_state* state) noexcept
 {
-    // TODO: implement
-    return ULIGHT_STATUS_OK;
+    if (state->source == nullptr && state->source_length != 0) {
+        return ULIGHT_STATUS_BAD_STATE;
+    }
+    if (state->token_buffer == nullptr || state->token_buffer_length == 0
+        || state->flush_tokens == nullptr) {
+        return ULIGHT_STATUS_BAD_BUFFER;
+    }
+    switch (state->lang) {
+    case ULIGHT_LANG_CPP:
+    case ULIGHT_LANG_MMML: break;
+    case ULIGHT_LANG_NONE: return ULIGHT_STATUS_BAD_LANG;
+    }
+
+    ulight::Non_Owning_Buffer<ulight_token> buffer { state->token_buffer,
+                                                     state->token_buffer_length,
+                                                     state->flush_tokens_data,
+                                                     state->flush_tokens };
+    // This may actually lead to undefined behavior.
+    // Counterpoint: it works on my machine.
+    const std::u8string_view source { std::launder(reinterpret_cast<const char8_t*>(state->source)),
+                                      state->source_length };
+    ulight::Global_Memory_Resource memory;
+    const ulight::Highlight_Options options = ulight::to_options(state->flags);
+
+    try {
+        const ulight::Status result
+            = ulight::highlight(buffer, source, ulight::Lang(state->lang), &memory, options);
+        return ulight_status(result);
+    } catch (const ulight::utf8::Unicode_Error&) {
+        return ULIGHT_STATUS_BAD_TEXT;
+    } catch (const std::bad_alloc&) {
+        return ULIGHT_STATUS_BAD_ALLOC;
+    } catch (...) {
+        return ULIGHT_STATUS_INTERNAL_ERROR;
+    }
 }
 
 ulight_status ulight_tokens_to_html(ulight_state* state) noexcept
 {
-    // TODO: implement
+
     return ULIGHT_STATUS_OK;
 }
 
-ulight_status ulight_source_to_html(ulight_state* state) noexcept
+// The NOLINT suppresses a bug: https://github.com/llvm/llvm-project/issues/132605
+ulight_status ulight_source_to_html(ulight_state* state) // NOLINT(bugprone-exception-escape)
+    noexcept
 {
-    const ulight_status to_tokens = ulight_source_to_tokens(state);
-    if (to_tokens != ULIGHT_STATUS_OK) {
-        return to_tokens;
+    using namespace std::literals;
+
+    if ((state->token_buffer == nullptr && state->token_buffer_length != 0) //
+        || state->text_buffer == nullptr || state->text_buffer_length == 0
+        || state->flush_text == nullptr) {
+        return ULIGHT_STATUS_BAD_BUFFER;
     }
+    if (state->html_tag_name == nullptr || state->html_tag_name_length == 0
+        || state->html_attr_name == nullptr || state->html_attr_name_length == 0) {
+        return ULIGHT_STATUS_BAD_STATE;
+    }
+
+    const std::string_view html_tag_name { state->html_tag_name, state->html_tag_name_length };
+    const std::string_view html_attr_name { state->html_attr_name, state->html_attr_name_length };
+
+    ulight::Non_Owning_Buffer<char> buffer { state->text_buffer, state->text_buffer_length,
+                                             state->flush_text_data, state->flush_text };
+
+    auto flush_text = // clang-format off
+    [&, previous_end = 0uz](const ulight_token* tokens, std::size_t amount) mutable  {
+        for (std::size_t i = 0; i < amount; ++i) {
+            const auto& t = tokens[i];
+            if (t.begin > previous_end) {
+                const std::string_view source_gap { state->source + t.begin, t.length };
+                buffer.append_range(source_gap);
+            }
+
+            const std::string_view id
+                = ulight::highlight_type_id(ulight::Highlight_Type(t.type));
+            const std::string_view source_part { state->source + t.begin, t.length };
+
+            buffer.push_back('<');
+            buffer.append_range(html_tag_name);
+            buffer.push_back(' ');
+            buffer.append_range(html_attr_name);
+            buffer.push_back(' ');
+            buffer.append_range(id);
+            buffer.push_back('>');
+            buffer.append_range(source_part);
+            buffer.push_back('<');
+            buffer.append_range(html_tag_name);
+            buffer.append_range("/>"sv);
+
+            previous_end = t.begin + t.length;
+        }
+    };
+
+    // clang-format on
+    state->flush_tokens_data = &flush_text;
+    state->flush_tokens = [](void* erased_flush_text, ulight_token* tokens, std::size_t amount) {
+        auto& flush_text_ref = *static_cast<decltype(flush_text)*>(erased_flush_text);
+        flush_text_ref(tokens, amount);
+    };
+
     return ulight_tokens_to_html(state);
 }
 
-//
-}
+} // extern "C"
