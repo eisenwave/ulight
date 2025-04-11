b60641c1abd7cdea2d82cce683ce2bcb701c7032
Eisenwave <me@eisenwave.net>
2025-04-11 20:57:28 +0200

    add length_if and length_if_not

--- include/ulight/impl/unicode_algorithm.hpp
+++ include/ulight/impl/unicode_algorithm.hpp
@@ -11,9 +11,10 @@ namespace ulight::utf8 {
 
 namespace detail {
 
-template <bool expected, typename F>
+template <typename F>
     requires std::is_invocable_r_v<bool, F, char32_t>
-std::size_t find_if(std::u8string_view str, F predicate)
+[[nodiscard]]
+std::size_t find_if(std::u8string_view str, F predicate, bool expected, std::size_t npos)
 {
     std::size_t code_units = 0;
     while (!str.empty()) {
@@ -24,7 +25,7 @@ std::size_t find_if(std::u8string_view str, F predicate)
         code_units += std::size_t(length);
         str.remove_prefix(std::size_t(length));
     }
-    return std::u8string_view::npos;
+    return npos;
 }
 
 } // namespace detail
@@ -35,9 +36,10 @@ std::size_t find_if(std::u8string_view str, F predicate)
 /// @throws Unicode_Error If decoding failed.
 template <typename F>
     requires std::is_invocable_r_v<bool, F, char32_t>
+[[nodiscard]]
 constexpr std::size_t find_if(std::u8string_view str, F predicate)
 {
-    return detail::find_if<true>(str, predicate);
+    return detail::find_if(str, predicate, true, std::u8string_view::npos);
 }
 
 /// @brief Returns the position of the first code point `c` in `str` for which
@@ -46,9 +48,28 @@ constexpr std::size_t find_if(std::u8string_view str, F predicate)
 /// @throws Unicode_Error If decoding failed.
 template <typename F>
     requires std::is_invocable_r_v<bool, F, char32_t>
+[[nodiscard]]
 constexpr std::size_t find_if_not(std::u8string_view str, F predicate)
 {
-    return detail::find_if<false>(str, predicate);
+    return detail::find_if(str, predicate, false, str.length());
+}
+
+/// @brief Like `find_if`, but returns `str.length()` instead of `npos`.
+template <typename F>
+    requires std::is_invocable_r_v<bool, F, char32_t>
+[[nodiscard]]
+constexpr std::size_t length_if(std::u8string_view str, F predicate)
+{
+    return detail::find_if(str, predicate, true, str.length());
+}
+
+/// @brief Like `find_if_not`, but returns `str.length()` instead of `npos`.
+template <typename F>
+    requires std::is_invocable_r_v<bool, F, char32_t>
+[[nodiscard]]
+constexpr std::size_t length_if_not(std::u8string_view str, F predicate)
+{
+    return detail::find_if(str, predicate, false, str.length());
 }
 
 } // namespace ulight::utf8
