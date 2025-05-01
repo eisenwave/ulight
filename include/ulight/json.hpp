48acfa784558fa0e7cbe90233ef95e0fd184e7c1
Eisenwave <me@eisenwave.net>
2025-05-01 20:29:51 +0200

    add convenience overload, add empty implementation for visitor members

--- include/ulight/json.hpp
+++ include/ulight/json.hpp
@@ -47,41 +47,33 @@ enum struct Error_Reaction : Underlying {
     // like the option to attempt error recovery.
 };
 
+ULIGHT_DIAGNOSTIC_PUSH()
+ULIGHT_DIAGNOSTIC_IGNORED("-Wunused-parameter")
+// NOLINTBEGIN(misc-unused-parameters)
+
 // NOLINTNEXTLINE(cppcoreguidelines-special-member-functions)
 struct JSON_Visitor {
     virtual ~JSON_Visitor() = default;
 
     /// @brief Invoked when a single-line comment (`// ...`) is matched.
     /// @param pos The position of the leading `/` character.
-    virtual void line_comment(
-        [[maybe_unused]] const Source_Position& pos,
-        [[maybe_unused]] std::u8string_view comment
-    )
-    {
-        // Ignore comment.
-    }
+    virtual void line_comment(const Source_Position& pos, std::u8string_view comment) { }
 
     /// @brief Invoked when a block comment (`/* ... */`) is matched.
     /// @param pos The position of the leading `/` character.
-    virtual void block_comment(
-        [[maybe_unused]] const Source_Position& pos,
-        [[maybe_unused]] std::u8string_view comment
-    )
-    {
-        // Ignore comment.
-    }
+    virtual void block_comment(const Source_Position& pos, std::u8string_view comment) { }
 
     /// @brief Invoked when matching literal characters within a string.
     /// These characters contain no `\` or control characters.
     /// @param pos The position of the first literal character.
-    virtual void literal(const Source_Position& pos, std::u8string_view chars) = 0;
+    virtual void literal(const Source_Position& pos, std::u8string_view chars) { }
 
     /// @brief Invoked when matching an escape sequence within a string
     /// and the `parse_escapes` option is `false`.
     /// @param pos The position of the leading `\`.
     /// @param escape The contents of the escape sequence,
     /// including the leading `\`.
-    virtual void escape(const Source_Position& pos, std::u8string_view escape) = 0;
+    virtual void escape(const Source_Position& pos, std::u8string_view escape) { }
     /// @brief Invoked when matching an escape sequence within a string
     /// and the `parse_escapes` option is `true`.
     /// @param pos The position of the leading `\`.
@@ -91,64 +83,68 @@ struct JSON_Visitor {
     /// Due to JSON only supporting four-digit `\u` escapes,
     /// the maximum code point is `U+FFFF`.
     virtual void escape(const Source_Position& pos, std::u8string_view escape, char32_t code_point)
-        = 0;
+    {
+    }
 
     /// @brief Invoked when a number is matched
     /// and the `parse_numbers` option is `false`.
     /// @param pos The position of the first character of the number.
     /// @param number The contents of the number.
-    virtual void number(const Source_Position& pos, std::u8string_view number) = 0;
+    virtual void number(const Source_Position& pos, std::u8string_view number) { }
     /// @brief Invoked when a number is matched
     /// and the `parse_numbers` option is `true`.
     /// @param pos The position of the first character of the number.
     /// @param number The contents of the number.
     /// @param value The parsed value of the number.
-    virtual void number(const Source_Position& pos, std::u8string_view number, double value) = 0;
+    virtual void number(const Source_Position& pos, std::u8string_view number, double value) { }
 
     /// @brief Invoked when `null` is matched.
     /// @param pos The position of the leading `n` character.
-    virtual void null(const Source_Position& pos) = 0;
+    virtual void null(const Source_Position& pos) { }
     /// @brief Invoked when `true` or `false` is matched.
     /// @param pos The position of the leading `t` or `f` character.
-    virtual void boolean(const Source_Position& pos, bool value) = 0;
+    virtual void boolean(const Source_Position& pos, bool value) { }
 
     /// @brief Invoked when a value string is entered.
     /// @param pos The position of the opening `"` character.
-    virtual void push_string(const Source_Position& pos) = 0;
+    virtual void push_string(const Source_Position& pos) { }
     /// @brief Invoked when a value string is exited.
     /// @param pos The position of the closing `"` character.
-    virtual void pop_string(const Source_Position& pos) = 0;
+    virtual void pop_string(const Source_Position& pos) { }
 
     /// @brief Invoked when a property string is entered.
     /// @param pos The position of the opening `"` character.
-    virtual void push_property(const Source_Position& pos) = 0;
+    virtual void push_property(const Source_Position& pos) { }
     /// @brief Invoked when a property string is exited.
     /// @param pos The position of the closing `"` character.
-    virtual void pop_property(const Source_Position& pos) = 0;
+    virtual void pop_property(const Source_Position& pos) { }
 
     /// @brief Invoked when an object is entered.
     /// @param pos The position of the opening `{` character.
-    virtual void push_object(const Source_Position& pos) = 0;
+    virtual void push_object(const Source_Position& pos) { }
     /// @brief Invoked when an object is exited.
     /// @param pos The position of the closing `}` character.
-    virtual void pop_object(const Source_Position& pos) = 0;
+    virtual void pop_object(const Source_Position& pos) { }
 
     /// @brief Invoked when an array is entered.
     /// @param pos The position of the opening `[` character.
-    virtual void push_array(const Source_Position& pos) = 0;
+    virtual void push_array(const Source_Position& pos) { }
     /// @brief Invoked when an array is exited.
     /// @param pos The position of the closing `]` character.
-    virtual void pop_array(const Source_Position& pos) = 0;
+    virtual void pop_array(const Source_Position& pos) { }
 
     /// @brief Invoked when a parse error occurs.
     /// @param pos The position of the character responsible for the error.
-    virtual Error_Reaction
-    error([[maybe_unused]] const Source_Position& pos, [[maybe_unused]] JSON_Error error)
+    virtual Error_Reaction error([[maybe_unused]] const Source_Position& pos, JSON_Error error)
     {
         return Error_Reaction::abort;
     }
 };
 
+// NOLINTEND(misc-unused-parameters)
+ULIGHT_DIAGNOSTIC_POP()
+
+/// @brief Options for JSON parsing.
 struct JSON_Options {
     /// @brief If `true`, `// ...` and `/* ... */` comments are allowed.
     /// Otherwise, comments result in `JSON_Error::comment`.
@@ -167,6 +163,10 @@ struct JSON_Options {
 /// @returns `true` if the file was parsed successfully, else `false`.
 /// More detailed error feedback can be obtained by overriding `JSON_Visitor::error`.
 bool parse_json(JSON_Visitor& visitor, std::u8string_view source, JSON_Options options = {});
+
+/// @brief Like the overload taking `std::u8string_view`,
+/// but taking `std::string_view` for compatibility.
+bool parse_json(JSON_Visitor& visitor, std::string_view source, JSON_Options options = {});
 
 } // namespace ulight
 
