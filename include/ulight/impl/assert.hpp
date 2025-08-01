82b9854d4f5202b82a4e9cf6d6c6abdb81ef93e3
Eisenwave <me@eisenwave.net>
2025-08-01 15:11:35 +0200

    allow for configurable assertion handle

--- include/ulight/impl/assert.hpp
+++ include/ulight/impl/assert.hpp
@@ -6,10 +6,7 @@
 
 #include "ulight/impl/platform.h"
 
-#ifdef ULIGHT_EXCEPTIONS
-#define ULIGHT_RAISE_ASSERTION_ERROR(...) (throw __VA_ARGS__)
-#else
-#define ULIGHT_RAISE_ASSERTION_ERROR(...) ::std::exit(3)
+#if !defined(ULIGHT_EXCEPTIONS) && !defined(ULIGHT_EMSCRIPTEN)
 #include <cstdlib> // for std::exit
 #endif
 
@@ -26,21 +23,50 @@ struct Assertion_Error {
     std::source_location location;
 };
 
+using Assert_Fail_Fn = void(const Assertion_Error& error);
+
+/// The default assertion handler.
+[[noreturn]]
+inline void handle_assertion([[maybe_unused]] const Assertion_Error& error)
+{
+#ifdef ULIGHT_EXCEPTIONS
+    throw error;
+#elif defined(ULIGHT_EMSCRIPTEN)
+    __builtin_trap();
+#else
+    ::std::exit(3);
+#endif
+};
+
+/// A pointer to a function which is invoked when an assertion fails.
+/// By default, this will perform default assertion handling,
+/// which is to throw, trap, or exist.
+/// However, reassigning this function pointer
+/// can be used to print additional information about the failed assertion.
+inline thread_local Assert_Fail_Fn* assertion_handler = handle_assertion;
+
+[[noreturn]]
+inline void assert_fail(const Assertion_Error& error)
+{
+    assertion_handler(error);
+    __builtin_trap();
+}
+
 // Expects an expression.
 // If this expression (after contextual conversion to `bool`) is `false`,
 // throws an `Assertion_Error` of type `expression`.
 #define ULIGHT_ASSERT(...)                                                                         \
     ((__VA_ARGS__) ? void()                                                                        \
-                   : ULIGHT_RAISE_ASSERTION_ERROR(::ulight::Assertion_Error {                      \
+                   : ::ulight::assert_fail(::ulight::Assertion_Error {                             \
                          ::ulight::Assertion_Error_Type::expression, u8## #__VA_ARGS__,            \
                          ::std::source_location::current() }))
 
 /// Expects a string literal.
 /// Unconditionally throws `Assertion_Error` of type `unreachable`.
 #define ULIGHT_ASSERT_UNREACHABLE(...)                                                             \
-    ULIGHT_RAISE_ASSERTION_ERROR(::ulight::Assertion_Error {                                       \
-        ::ulight::Assertion_Error_Type::unreachable, ::std::u8string_view(__VA_ARGS__),            \
-        ::std::source_location::current() })
+    ::ulight::assert_fail(::ulight::Assertion_Error { ::ulight::Assertion_Error_Type::unreachable, \
+                                                      ::std::u8string_view(__VA_ARGS__),           \
+                                                      ::std::source_location::current() })
 
 #define ULIGHT_IS_CONTEXTUALLY_BOOL_CONVERTIBLE(...) requires { (__VA_ARGS__) ? 1 : 0; }
 
