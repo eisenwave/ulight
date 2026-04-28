9f75c564cf2c233a078963f26d51d4f1d17787eb
Eisenwave <me@eisenwave.net>
2026-04-28 22:45:18 +0200

    Refactor some more character tests

--- include/ulight/impl/lang/cpp_chars.hpp
+++ include/ulight/impl/lang/cpp_chars.hpp
@@ -7,21 +7,7 @@
 namespace ulight {
 
 /// @brief Returns `true` iff `c` is in the set `[A-Za-z_]`.
-inline constexpr Charset256 is_cpp_ascii_identifier_start_set = is_ascii_xid_start | u8'_';
-
-/// @brief Returns `true` iff `c` is in the set `[A-Za-z_]`.
-[[nodiscard]]
-constexpr bool is_cpp_ascii_identifier_start(const char8_t c) noexcept
-{
-    return c == u8'_' || is_ascii_xid_start(c);
-}
-
-/// @brief Returns `true` iff `c` is in the set `[A-Za-z_]`.
-[[nodiscard]]
-constexpr bool is_cpp_ascii_identifier_start(const char32_t c) noexcept
-{
-    return c == U'_' || is_ascii_xid_start(c);
-}
+inline constexpr Charset256 is_cpp_ascii_identifier_start = is_ascii_xid_start | u8'_';
 
 inline constexpr struct Is_CPP_Identifier_Start {
     static constexpr bool operator()(const char8_t c) = delete;
@@ -35,21 +21,7 @@ inline constexpr struct Is_CPP_Identifier_Start {
 } is_cpp_identifier_start;
 
 /// @brief Returns `true` iff `c` is in the set `[A-Za-z0-9_]`.
-inline constexpr Charset256 is_cpp_ascii_identifier_continue_set = is_ascii_xid_continue;
-
-/// @brief Returns `true` iff `c` is in the set `[A-Za-z0-9_]`.
-[[nodiscard]]
-constexpr bool is_cpp_ascii_identifier_continue(const char8_t c) noexcept
-{
-    return is_ascii_xid_continue(c);
-}
-
-/// @brief Returns `true` iff `c` is in the set `[A-Za-z0-9_]`.
-[[nodiscard]]
-constexpr bool is_cpp_ascii_identifier_continue(const char32_t c) noexcept
-{
-    return is_ascii_xid_continue(c);
-}
+inline constexpr Charset256 is_cpp_ascii_identifier_continue = is_ascii_xid_continue;
 
 inline constexpr struct Is_CPP_Identifier_Continue {
     static constexpr bool operator()(const char8_t c) = delete;
